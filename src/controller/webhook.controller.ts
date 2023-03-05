71a3f28170ebe2a0022b387ca76678cf71731a4d
yash-chace <yash.raj@lockrfinance.com>
2023-03-05 18:34:48 +0530

    integrated update status on commit on pr

--- src/controller/webhook.controller.ts
+++ src/controller/webhook.controller.ts
@@ -1,18 +1,48 @@
 import { Router, Request, Response } from "express";
+import TickeModel from "../model/ticket.model";
 
 const router = Router();
 
 router.post("/commit", async (req: Request, res: Response) => {
   try {
-    console.log(req.body);
-    return res.status(200).send(req.body);
+    let gitHubBranch = req.body.ref.split("/")[3];
+    let tickeArray = gitHubBranch.split("-");
+    let tickeNumber = `${tickeArray[0]}-${tickeArray[1]}`;
+
+    let currentTicket = await TickeModel.findOne({
+      issueId: tickeNumber.toUpperCase(),
+    });
+    if (currentTicket && currentTicket.status == "TODO") {
+      currentTicket.status = "INPROGRESS";
+      currentTicket = await currentTicket.save();
+    }
+
+    return res.status(200).send({});
   } catch (error) {}
 });
 
 router.post("/pr", async (req: Request, res: Response) => {
   try {
-    console.log(req.body);
-    return res.status(200).send(req.body);
+    let ticket = req.body.pull_request.title
+      .trim()
+      .split(" ")[0]
+      .replace(/\[|\]/g, "");
+    let currentTicket = await TickeModel.findOne({
+      issueId: ticket,
+    });
+    if (currentTicket && req.body.action == "opened") {
+      currentTicket.status = "IN_DEV_REVIEW";
+      currentTicket = await currentTicket.save();
+    } else if (currentTicket && req.body.pull_request.merged) {
+      currentTicket.status = "DONE";
+      currentTicket = await currentTicket.save();
+    } else if (req.body.action == "closed") {
+      //Need to update link section in ticket detials
+    }
+    console.log(ticket);
+    console.log(currentTicket);
+    console.log(req.body.pull_request.merged);
+    return res.status(200).send({});
   } catch (error) {}
 });
 
