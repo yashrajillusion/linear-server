986f08a543725524fd33aaed30f0b9863c814af1
yash-chace <yash.raj@lockrfinance.com>
2023-03-05 14:45:57 +0530

    update hooks for commit on github

--- src/controller/webhook.controller.ts
+++ src/controller/webhook.controller.ts
@@ -2,7 +2,14 @@ import { Router, Request, Response } from "express";
 
 const router = Router();
 
-router.post("/", async (req: Request, res: Response) => {
+router.post("/commit", async (req: Request, res: Response) => {
+  try {
+    console.log(req.body);
+    return res.status(200).send(req.body);
+  } catch (error) {}
+});
+
+router.post("/pr", async (req: Request, res: Response) => {
   try {
     console.log(req.body);
     return res.status(200).send(req.body);
