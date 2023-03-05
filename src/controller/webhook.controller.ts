83c6b8525fca76e0efda026a603f4f1778f5747f
yash-chace <yash.raj@lockrfinance.com>
2023-03-05 16:50:53 +0530

    working on webhooks

--- src/controller/webhook.controller.ts
+++ src/controller/webhook.controller.ts
@@ -4,6 +4,8 @@ const router = Router();
 
 router.post("/commit", async (req: Request, res: Response) => {
   try {
+    //req.ref
+    console.log(req.body.ref);
     console.log(req.body);
     return res.status(200).send(req.body);
   } catch (error) {}
@@ -11,9 +13,15 @@ router.post("/commit", async (req: Request, res: Response) => {
 
 router.post("/pr", async (req: Request, res: Response) => {
   try {
-    console.log(req.body);
+    //req.body.pull_request.title
+    // merged
+    //req.body.action
+    console.log(req.body.pull_request.title);
+    console.log(req.body.pull_request.merged);
+    console.log(req.body.action);
     return res.status(200).send(req.body);
   } catch (error) {}
 });
+//yashraj/lead-4708-map-gocomet-migration
 
 export default router;
